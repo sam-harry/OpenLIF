a990772c3ab96b15496800343f5639a0bc09720d
Sam Harry <sam.harry@pm.me>
2022-05-19 17:48:18 -0700

    Alter run.py in examples to __name__=='__main__'

--- examples/solitary_wave_breaking/run.py
+++ examples/solitary_wave_breaking/run.py
@@ -8,64 +8,68 @@ sys.path.append('../../src') # or wherever the src directory is
 from openlif import lif as olif
 from openlif import find_homography, find_physical, find_interpolation
 
-# load camera calibration paramaters
-matrx = np.loadtxt('matrx.csv',delimiter=',')
-coeff = np.loadtxt('coeff.csv',delimiter=',')
-params = (matrx,coeff)
+def main():
+  # load camera calibration paramaters
+  matrx = np.loadtxt('matrx.csv',delimiter=',')
+  coeff = np.loadtxt('coeff.csv',delimiter=',')
+  params = (matrx,coeff)
 
-# open images as video capture
-cap = cv.VideoCapture('solitary_wave_breaking.avi')
+  # open images as video capture
+  cap = cv.VideoCapture('solitary_wave_breaking.avi')
 
-# find water-air surface profile
-# in this case it is the upper most edge of the illuminated block
-# which is the 'low' interface in image coordinates
-kwargs = {
-  'N':None, # defaults to None
-  'calibration_params':params, # defaults to None
-  'k':4, # defaults to 3 - need more sensitivity for these images
-  'use_phase_mask':True, # defaults to True
-  'connected':True, # defaults to True
-  'use_column_max':False, # defaults to False
-}
-surface_air = olif(cap,'low','lower',**kwargs)
+  # find water-air surface profile
+  # in this case it is the upper most edge of the illuminated block
+  # which is the 'low' interface in image coordinates
+  kwargs = {
+    'N':None, # defaults to None
+    'calibration_params':params, # defaults to None
+    'k':4, # defaults to 3 - need more sensitivity for these images
+    'use_phase_mask':True, # defaults to True
+    'connected':True, # defaults to True
+    'use_column_max':False, # defaults to False
+  }
+  surface_air = olif(cap,'low','lower',**kwargs)
 
-# overlay surface line on original images
-try:
-  os.mkdir('overlay_lif')
-except FileExistsError:
-  pass
+  # overlay surface line on original images
+  try:
+    os.mkdir('overlay_lif')
+  except FileExistsError:
+    pass
 
-cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-for frame_number in range(surface_air.shape[0]):
-  _, img = cap.read()
-  for i, j in enumerate(surface_air[frame_number,:]):
-    img[int(j),i,:] = [0,0,2**16-1] #red
+  cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+  for frame_number in range(surface_air.shape[0]):
+    _, img = cap.read()
+    for i, j in enumerate(surface_air[frame_number,:]):
+      img[int(j),i,:] = [0,0,2**16-1] #red
 
-  cv.imwrite(f'overlay_lif/overlay_lif_{frame_number+1:04d}.png',img)
+    cv.imwrite(f'overlay_lif/overlay_lif_{frame_number+1:04d}.png',img)
 
-# load calibration image and find homography
-calib = cv.imread('calib.tif',cv.IMREAD_GRAYSCALE)
-H, board_size = find_homography(calib,(9,6),25.4)
+  # load calibration image and find homography
+  calib = cv.imread('calib.tif',cv.IMREAD_GRAYSCALE)
+  H, board_size = find_homography(calib,(9,6),25.4)
 
-# convert from pixel space to physical space
-xdata_air, ydata_air, R_air = find_physical(H,surface_air)
-xi, y, dx = find_interpolation( xdata_air,
-                               -ydata_air.filled(fill_value=np.nan),0.3)
+  # convert from pixel space to physical space
+  xdata_air, ydata_air, R_air = find_physical(H,surface_air)
+  xi, y, dx = find_interpolation( xdata_air,
+                                 -ydata_air.filled(fill_value=np.nan),0.3)
 
-# calibration detection is backwards from what we want - reverse axis
-xi *= -1
-y  *= -1
+  # calibration detection is backwards from what we want - reverse axis
+  xi *= -1
+  y  *= -1
 
-# generate colormesh plot
-dt = 1/125
-ti = np.arange(y.shape[0])
-T, X = np.meshgrid(ti,xi,indexing='ij')
+  # generate colormesh plot
+  dt = 1/125
+  ti = np.arange(y.shape[0])
+  T, X = np.meshgrid(ti,xi,indexing='ij')
 
-cmap = plt.pcolormesh(X*dx,T*dt,y)
+  cmap = plt.pcolormesh(X*dx,T*dt,y)
 
-plt.xlabel('position (mm)')
-plt.ylabel('time (s)')
-plt.colorbar(cmap,label='water surface elevation (mm)')
-plt.tight_layout()
-plt.savefig('surface.png',dpi=400)
+  plt.xlabel('position (mm)')
+  plt.ylabel('time (s)')
+  plt.colorbar(cmap,label='water surface elevation (mm)')
+  plt.tight_layout()
+  plt.savefig('surface.png',dpi=400)
 
+
+if __name__ == '__main__':
+  main()
